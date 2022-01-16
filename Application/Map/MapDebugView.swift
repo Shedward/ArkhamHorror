10635af7786da91a7b02f78499ddd05116433ef4
Vlad Maltsev <maltsevvn@m2.ru>
2022-01-16 18:34:06 +0300

    State and showing errors.

--- Application/Map/MapDebugView.swift
+++ Application/Map/MapDebugView.swift
@@ -6,20 +6,42 @@
 //
 
 import SwiftUI
+import ArkhamHorror
 
 struct MapDebugView: View {
+	enum ViewState {
+		case empty
+		case failed(Error)
+		case loaded(Map)
+	}
+
 	@State
-	private var text: String = "Drop map here"
+	private var state: ViewState = .empty
 
     var body: some View {
-		Text(text)
-			.frame(minWidth: 500, minHeight: 500)
-			.onDrop(
-				of: MapDropDelegate.supportedTypes,
-				delegate: MapDropDelegate { result in
-					self.text = String(describing: result)
+		ZStack {
+			switch state {
+			case .empty:
+				Text("Drop map here")
+			case .failed(let error):
+				Text(String(describing: error))
+					.foregroundColor(.red)
+			case .loaded:
+				Text("Map loaded")
+			}
+		}
+		.frame(minWidth: 500, minHeight: 500)
+		.onDrop(
+			of: MapDropDelegate.supportedTypes,
+			delegate: MapDropDelegate { result in
+				switch result {
+				case .success(let map):
+					self.state = .loaded(map)
+				case .failure(let error):
+					self.state = .failed(error)
 				}
-			)
+			}
+		)
     }
 }
 
