b0bd0031e77a22f8387504920602a4c66b920e30
Vlad Maltsev <maltsevvn@m2.ru>
2022-03-06 00:19:22 +0300

    Added docs.

--- Domain/Cards/Sources/Cards/Events/ActionExpressions/DrawSpecial.swift
+++ Domain/Cards/Sources/Cards/Events/ActionExpressions/DrawSpecial.swift
@@ -17,6 +17,12 @@ struct DrawSpecial: Expression {
 }
 
 struct DrawSpecialParser: ExpressionParser {
+	let head = "drawSpecial"
+	let doc = ExpressionDoc(
+		signature: #"(drawSpecial "cardId":Event.Id):Void"#,
+		description: "User draws special card with specific id",
+		example: #"(drawSpecial "some_special_card")"#
+	)
 	func parse(
 		_ reader: ExpressionParameterReader<EventContext>
 	) throws -> AnyExpression<EventContext, Void> {
