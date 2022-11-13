eea0d2f6757c3da3666117e2cc999ad71723dfd4
Vlad Maltsev <v.maltsev@thesollution.com>
2022-11-13 19:52:04 +0300

    Structural refactoring of ConfigureCharacterView

--- Application/Gameplay/SelectCharacters/ConfigureCharacterView.swift
+++ Application/Gameplay/SelectCharacters/ConfigureCharacterView.swift
@@ -44,95 +44,128 @@ struct ConfigureCharacterView: View {
     var body: some View {
         HStack(alignment: .top, spacing: 24) {
             VStack(spacing: 12) {
-                CharacterPortrait(size: .big)
-                VStack {
-                    Text(name)
-                        .styled(.Design.Menu.h2)
-                        .multilineTextAlignment(.center)
-                    Text(profession)
-                        .styled(.Design.Menu.story)
-                }
-                .padding(.bottom)
-                MenuHSeparator()
-                    .frame(maxWidth: 50)
-                Text(story)
-                    .styled(.Design.Menu.story)
-                if !roles.isEmpty {
-                    MenuHSeparator()
-                        .frame(maxWidth: 50)
-                    VStack(alignment: .leading, spacing: 16) {
-                        ForEach(roles, id: \.name) { role in
-                            let name = Localized.string("%@ - ", arguments: role.name)
-                            let formattedName = AttributedString(name)
-                                .styled(.Design.Menu.keyword)
-                            let formattedDescription = AttributedString(role.description)
-                                .styled(.Design.Menu.body)
-                            Text(formattedName + formattedDescription)
-                        }
-                    }
-                    .padding(.top)
-                }
+                characterSection
+                    .padding(.bottom)
+                storySection
+                rolesSection
             }
             .frame(maxWidth: CharacterPortrait.width(for: .big) * 1.5)
             MenuVSeparator().padding([.top, .bottom])
             VStack(alignment: .center) {
-                if !skills.isEmpty {
-                    Text(Localized.string("Stats"))
-                        .styled(.Design.Menu.h3)
-                    HStack(spacing: 32) {
-                        VStack(spacing: 0) {
-                            HStack {
-                                Image("health")
-                                    .foregroundColor(Color(.Design.Content.main))
-                                Text(Localized.number(stats.health))
-                                    .styled(.Design.Menu.h1)
-                            }
-                            HStack {
-                                Image("mind")
-                                    .foregroundColor(Color(.Design.Content.main))
-                                Text(Localized.number(stats.mind))
-                                    .styled(.Design.Menu.h1)
-                            }
-                        }
-                        .padding(
-                            EdgeInsets(
-                                top: 8,
-                                leading: 16,
-                                bottom: 8,
-                                trailing: 24
-                            )
-                        )
-                        .border(Color(.Design.Content.main))
-
-                        HStack(alignment: .bottom, spacing: 8) {
-                            ForEach(skills, id: \.icon) { skill in
-                                VStack(alignment: .center, spacing: 0) {
-                                    Image(skill.icon)
-                                        .foregroundColor(Color(.Design.Content.main))
-                                    Text(Localized.number(skill.value))
-                                        .styled(.Design.Menu.h1)
-                                }
-                            }
-                            Text("+ 2")
-                                .styled(.Design.Menu.h1)
-                                .padding(.leading)
-                        }
-                        .padding()
-                        .border(Color(.Design.Content.main))
-                    }
-                }
+                statsSection
                 UnderConstructionView(textStyle: .Design.Menu.debug)
-                MenuButton(title: Localized.string("Add"), icons: .rightIcon("arrow.up.right"))
+                buttonsSection
             }
         }
+    }
+
+    var characterSection: some View {
+        VStack {
+            CharacterPortrait(size: .big)
+            VStack {
+                Text(name)
+                    .styled(.Design.Menu.h2)
+                    .multilineTextAlignment(.center)
+                Text(profession)
+                    .styled(.Design.Menu.story)
+            }
+        }
+    }
+
+    var storySection: some View {
+        VStack {
+            MenuHSeparator()
+                .frame(maxWidth: 50)
+            Text(story)
+                .styled(.Design.Menu.story)
+        }
+    }
+
+    var rolesSection: some View {
+        VStack {
+            if !roles.isEmpty {
+                MenuHSeparator()
+                    .frame(maxWidth: 50)
+                VStack(alignment: .leading, spacing: 16) {
+                    ForEach(roles, id: \.name) { role in
+                        let name = Localized.string("%@ - ", arguments: role.name)
+                        let formattedName = AttributedString(name)
+                            .styled(.Design.Menu.keyword)
+                        let formattedDescription = AttributedString(role.description)
+                            .styled(.Design.Menu.body)
+                        Text(formattedName + formattedDescription)
+                    }
+                }
+                .padding(.top)
+            }
+        }
+    }
+
+    var statsSection: some View {
+        VStack {
+            if !skills.isEmpty {
+                Text(Localized.string("Stats"))
+                    .styled(.Design.Menu.h3)
+                HStack(spacing: 32) {
+                    VStack(spacing: 0) {
+                        HStack {
+                            Image("health")
+                                .foregroundColor(Color(.Design.Content.main))
+                            Text(Localized.number(stats.health))
+                                .styled(.Design.Menu.h1)
+                        }
+                        HStack {
+                            Image("mind")
+                                .foregroundColor(Color(.Design.Content.main))
+                            Text(Localized.number(stats.mind))
+                                .styled(.Design.Menu.h1)
+                        }
+                    }
+                    .padding(
+                        EdgeInsets(
+                            top: 8,
+                            leading: 16,
+                            bottom: 8,
+                            trailing: 24
+                        )
+                    )
+                    .border(Color(.Design.Content.main))
+
+                    HStack(alignment: .bottom, spacing: 8) {
+                        ForEach(skills, id: \.icon) { skill in
+                            VStack(alignment: .center, spacing: 0) {
+                                Image(skill.icon)
+                                    .foregroundColor(Color(.Design.Content.main))
+                                Text(Localized.number(skill.value))
+                                    .styled(.Design.Menu.h1)
+                            }
+                        }
+                        Text("+ 2")
+                            .styled(.Design.Menu.h1)
+                            .padding(.leading)
+                    }
+                    .padding()
+                    .border(Color(.Design.Content.main))
+                }
+            }
+        }
+    }
+
+    var buttonsSection: some View {
+        HStack {
+            MenuButton(title: Localized.string("Back"), icons: .leftIcon("arrow.backward"))
+            Spacer()
+            MenuButton(title: Localized.string("Add"), icons: .rightIcon("arrow.up.right"))
+        }
+        .padding([.leading, .trailing], 128)
     }
 }
 
 struct ConfigureCharacterView_Previews: PreviewProvider {
     static var previews: some View {
         ConfigureCharacterView.mock()
-        .padding(32)
-        .background(Color(.Design.Background.main))
-        .ignoresSafeArea()
+            .padding(32)
+            .background(Color(.Design.Background.main))
+            .ignoresSafeArea()
     }
 }
