5fa0f495d79daa5dee363ef55288733b10782563
Vlad Maltsev <v.maltsev@thesollution.com>
2023-03-05 22:55:25 +0300

    Debug helpers.

---
+++ Utils/Prelude/Sources/Prelude/Helpers/Debug.swift
@@ -0,0 +1,14 @@
+//
+//  Debug.swift
+//  
+//
+//  Created by Vladislav Maltsev on 05.03.2023.
+//
+
+import Dispatch
+import Foundation
+
+@available(*, deprecated, message: "Debug only")
+public func delayToMain(_ time: TimeInterval, action: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(Int(time * 1000)), execute: action)
+}
